8afec7908adf6e15d68e7b7400992b8d3058cbfc
Ignacio Soler <ignaciojsoler@gmail.com>
2025-04-03 18:39:42 -0300

    3 - Abstract Factory - Solución a la tarea

--- 01-creacionales/03.2-abstract-factory.ts
+++ 01-creacionales/03.2-abstract-factory.ts
@@ -39,24 +39,37 @@ interface Engine {
 
 // 2. Clases Concretas de Productos
 
-class ElectricCar {
+class ElectricCar implements Vehicle {
   // Implementación del método assemble
   // 'Ensamblando un auto eléctrico'
+  assemble(): void {
+    console.log("Ensamblando un auto de eléctrico");
+  }
 }
 
-class GasCar {
+class GasCar implements Vehicle {
   // Implementación del método assemble
   // 'Ensamblando un auto de combustión'
+  assemble(): void {
+    console.log("Ensamblando un auto de combustión");
+  }
 }
 
-class ElectricEngine {
+class ElectricEngine implements Engine {
   // Implementación del método start
   // 'Arrancando motor eléctrico'
+  start(): void {
+    console.log("Arrancando motor eléctrico")
+  }
+
 }
 
-class GasEngine {
+class GasEngine implements Engine {
   // Implementación del método start
   // 'Arrancando motor de combustión'
+  start(): void {
+    console.log("Arrancando motor de combustión")
+  }
 }
 
 // 3. Interfaz de la Fábrica Abstracta
@@ -70,10 +83,23 @@ interface VehicleFactory {
 
 class ElectricVehicleFactory implements VehicleFactory {
   // Implementación de los métodos createVehicle y createEngine
+  createVehicle(): Vehicle {
+    return new ElectricCar();
+  }
+  createEngine(): Engine {
+    return new ElectricEngine();
+  }
+  
 }
 
 class GasVehicleFactory implements VehicleFactory {
   // Implementación de los métodos createVehicle y createEngine
+  createVehicle(): Vehicle {
+    return new GasCar();
+  }
+  createEngine(): Engine {
+    return new GasEngine();
+  }
 }
 
 // 5. Código Cliente
