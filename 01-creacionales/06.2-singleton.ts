060c28c933208fa85a2251140622f115d11e5ce0
Ignacio <70725223+ignaciojsoler@users.noreply.github.com>
2025-04-06 18:56:50 -0300

    6 - Singleton tarea

--- 01-creacionales/06.2-singleton.ts
+++ 01-creacionales/06.2-singleton.ts
@@ -15,22 +15,32 @@ class DatabaseConnection {
   private connected: boolean = false;
 
   // Constructor privado para evitar instancias directas
-  private constructor() {}
+  private constructor() {
+    this.connected = false;
+  }
 
   // Método estático para obtener la instancia única
   public static getInstance(): DatabaseConnection {
     // Completar: implementar el patrón Singleton
-    throw new Error('Method not implemented.');
+    if (!DatabaseConnection.instance) {
+      DatabaseConnection.instance = new DatabaseConnection();
+    }
+
+    return DatabaseConnection.instance
   }
 
   // Método para conectar a la base de datos
   public connect(): void {
     // Completar: si no está conectado, mostrar mensaje de conexión
+    this.connected = true;
+    console.log("Está conectada")
   }
 
   // Método para desconectar de la base de datos
   public disconnect(): void {
     // Completar: desconectar y mostrar mensaje de desconexión
+    this.connected = false;
+    console.log("Está desconectada")
   }
 }
 
