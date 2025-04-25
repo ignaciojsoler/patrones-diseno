73a622932684f2eb42900aa6abc34a084eaa5b3c
Ignacio Soler <ignaciojsoler@gmail.com>
2025-04-25 15:11:45 -0300

    2 - Bridge - Patron y tarea

--- 02-estructurales/02-bridge.ts
+++ 02-estructurales/02-bridge.ts
@@ -9,3 +9,72 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+
+interface Ability {
+    use(): void;
+}
+
+class SwordAttack implements Ability {
+    use(): void {
+        console.log('Sword attack!');
+    }
+}
+
+class AxeAttack implements Ability {
+    use(): void {
+        console.log('Axe brutally attack!');
+    }
+}
+
+class MagicSpell implements Ability {
+    use(): void {
+        console.log('Magic spell!');
+    }
+}
+
+class FireballSpell implements Ability {
+    use(): void {
+        console.log('Fireball spell!');
+    }
+}
+
+abstract class Character {
+    protected ability: Ability;
+
+    constructor(ability: Ability) {
+        this.ability = ability;
+    }
+
+    setAbility(ability: Ability): void {
+        this.ability = ability;
+    }
+
+    abstract performAbility(): void;
+}
+
+class Warrior extends Character {
+    override performAbility(): void {
+        console.log('Warrior is ready to fight!');
+        this.ability.use();
+    }
+}
+
+class Mage extends Character {
+    override performAbility(): void {
+        console.log('Mage is ready to cast spells!');
+        this.ability.use();
+    }
+}
+
+function main() {
+    const warrior = new Warrior(new SwordAttack());
+    warrior.performAbility();
+
+    warrior.setAbility(new AxeAttack());
+    warrior.performAbility();
+
+    const mage = new Mage(new FireballSpell());
+    mage.performAbility();
+}
+
+main();
